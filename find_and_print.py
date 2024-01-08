bafc0fd8c84d3e8e1bc5a258a4a6d04896601c5d
erichestella <erichestella05@gmail.com>
2024-01-09 03:37:57 +0800

    creating a function to defined which is the largest number among the three numbers entered

--- find_and_print.py
+++ find_and_print.py
@@ -8,22 +8,39 @@ chisato.geometry ("800x600")
 chisato.config(bg="pink")
 chisato.resizable(False, False)
 
+# defines which is the largest number among the three numbers entered
+def press():
+   first_number = float(enter_firstnumber.get())
+   second_number = float(enter_secondnumber.get())
+   third_number = float(enter_thirdnumber.get())
 
-# button to press if the three numbers are entered 
-press_button= customtkinter.CTkButton(chisato, text='ENTER', text_color='black')
+   if first_number > second_number and first_number > third_number:
+      print(f'{first_number} is the largest')
+   elif second_number > first_number and second_number > third_number:
+      print(f'{second_number} is the largest')
+   elif third_number > first_number and third_number > second_number:
+      print(f'{third_number} is the largest')
+
+   else: 
+      print("NUMBERS CAN'T DEFINED, PLEASE TRY AGAIN")
+
+
+
+# button to press if the three numbers are done entering 
+press_button= customtkinter.CTkButton(chisato, text='ENTER', text_color='black', command=press)
 press_button.place(x=30, y=40)
 
 # first number to enter
-enter_number=customtkinter.CTkEntry(chisato, width=200, height=30)
-enter_number.place(x= 100, y=100)
+enter_firstnumber=customtkinter.CTkEntry(chisato, width=200, height=30)
+enter_firstnumber.place(x= 100, y=100)
 
 # second number to enter 
-enter_number=customtkinter.CTkEntry(chisato, width=200, height=30)
-enter_number.place(x= 500, y=100)
+enter_secondnumber=customtkinter.CTkEntry(chisato, width=200, height=30)
+enter_secondnumber.place(x= 500, y=100)
 
 # third number to enter 
-enter_number=customtkinter.CTkEntry(chisato, width=200, height=30)
-enter_number.place(x= 300, y=100)
+enter_thirdnumber=customtkinter.CTkEntry(chisato, width=200, height=30)
+enter_thirdnumber.place(x= 300, y=100)
 
 
 
